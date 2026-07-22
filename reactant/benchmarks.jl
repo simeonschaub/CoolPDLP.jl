442adec8501c8d39f8c34fb00184c4928bce8ffe
Simeon SCHAUB <simeon.schaub@node07.cluster.local>
2026-07-22 10:34:12 +0200

    wip

--- reactant/benchmarks.jl
+++ reactant/benchmarks.jl
@@ -1,14 +1,8 @@
-### A Pluto.jl notebook ###
-# v1.0.1
-
-using Markdown
-using InteractiveUtils
-
 # ╔═╡ f30b45e9-4f21-43ba-866a-5acead85ccad
 begin
-	using Revise
-	#eval(:(import Pkg; Pkg.develop("CoolPDLP")))
-	using CoolPDLP, MinimumCostFlows
+    using Revise
+    #eval(:(import Pkg; Pkg.develop("CoolPDLP")))
+    using CoolPDLP, MinimumCostFlows
 end
 
 # ╔═╡ 03db4d24-f759-4d65-a0b3-b9058ea4091d
@@ -48,43 +42,43 @@ using MinimumCostFlows: One, Zero
 function two_per_col_spmv!(c::AbstractVector{T}, (; colidx)::Matrix2PerRowPM{I}, b::AbstractVector{T}, α::Number, β::Number) where {T <: Number, I <: Integer}
     i = (blockIdx().x - 1i32) * blockDim().x + threadIdx().x
     @inbounds @fastmath if i <= length(c) ÷ 2
-		ptr = reinterpret(Core.LLVMPtr{NTuple{4, Base.VecElement{I}}, AS.Global}, pointer(colidx))
-		j = SIMD.Vec(CUDA.unsafe_cached_load(ptr, i))
+        ptr = reinterpret(Core.LLVMPtr{NTuple{4, Base.VecElement{I}}, AS.Global}, pointer(colidx))
+        j = SIMD.Vec(CUDA.unsafe_cached_load(ptr, i))
         bⱼ = vgathera((pointer(b, 0i32)) + j * Int32(sizeof(T)))
 
-		l = shufflevector(bⱼ, Val((0, 1)))
-		sl = sum(SIMD.Vec{2, T}((1, -1)) * l)
+        l = shufflevector(bⱼ, Val((0, 1)))
+        sl = sum(SIMD.Vec{2, T}((1, -1)) * l)
         c[i] = α * sl + β * c[i]
-		u = shufflevector(bⱼ, Val((2, 3)))
+        u = shufflevector(bⱼ, Val((2, 3)))
         su = sum(SIMD.Vec{2, T}((1, -1)) * u)
         c[i + 1i32] = α * su + β * c[i + 1i32]
     end
-	return nothing
+    return nothing
 end
 
 function two_per_col_spmv!(c::AbstractVector{T}, (; colidx, vals)::Matrix2PerRow{T, I}, b::AbstractVector{T}, α::Number, β::Number) where {T <: Number, I <: Integer}
     i = (blockIdx().x - 1i32) * blockDim().x + threadIdx().x
     @inbounds @fastmath if i <= length(c) ÷ 2
-		ptr = reinterpret(Core.LLVMPtr{NTuple{4, Base.VecElement{I}}, AS.Global}, pointer(colidx))
-		j = SIMD.Vec(CUDA.unsafe_cached_load(ptr, i))
+        ptr = reinterpret(Core.LLVMPtr{NTuple{4, Base.VecElement{I}}, AS.Global}, pointer(colidx))
+        j = SIMD.Vec(CUDA.unsafe_cached_load(ptr, i))
         bⱼ = vgathera((pointer(b, 0i32)) + j * Int32(sizeof(T)))
-		val_ptr = reinterpret(Core.LLVMPtr{NTuple{4, Base.VecElement{T}}, AS.Global}, pointer(vals))
-		val = SIMD.Vec(CUDA.unsafe_cached_load(val_ptr, i))
-		prod = val * bⱼ
+        val_ptr = reinterpret(Core.LLVMPtr{NTuple{4, Base.VecElement{T}}, AS.Global}, pointer(vals))
+        val = SIMD.Vec(CUDA.unsafe_cached_load(val_ptr, i))
+        prod = val * bⱼ
 
-		sl = sum(shufflevector(prod, Val((0, 1))))
+        sl = sum(shufflevector(prod, Val((0, 1))))
         c[i] = α * sl + β * c[i]
-		su = sum(shufflevector(prod, Val((2, 3))))
+        su = sum(shufflevector(prod, Val((2, 3))))
         c[i + 1i32] = α * su + β * c[i + 1i32]
     end
-	return nothing
+    return nothing
 end
 
 function LinearAlgebra.mul!(c::CuVector{T}, A::Union{Matrix2PerRow{T, I}, Matrix2PerRowPM{I}}, b::CuVector{T}, α::Number, β::Number) where {T <: Number, I <: Integer}
     α_is_one = isone(α)
     β_is_zero = iszero(β)
-	threads = 768
-	blocks = cld(length(c) ÷ 2, threads)
+    threads = 768
+    blocks = cld(length(c) ÷ 2, threads)
     if α_is_one && β_is_zero
         @cuda threads=threads blocks=blocks two_per_col_spmv!(c, A, b, One(), Zero())
     elseif α_is_one
@@ -100,49 +94,61 @@ end
 using BenchmarkTools
 
 let b = CuArray{Float32}(b_test_left)
-	A = adapt(CUDABackend(), A_lpt)
-	c = similar(b, length(b_test))
-	@benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+    A = adapt(CUDABackend(), A_lpt)
+    c = similar(b, length(b_test))
+    @benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
 end
 
 let b = CuArray{Float32}(b_test_left)
-	A = adapt(CUDABackend(), Matrix2PerRow{Float32}(A_lpt))
-	c = similar(b, length(b_test))
-	@benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+    A = adapt(CUDABackend(), Matrix2PerRow{Float32}(A_lpt))
+    c = similar(b, length(b_test))
+    @benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
 end
 
 let b = CuArray{Float32}(b_test_left)
-	A = CuSparseMatrixCSR(SparseMatrixCSC{Float32}(A_lpt))
-	c = similar(b, length(b_test))
-	@benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+    A = CuSparseMatrixCSR(SparseMatrixCSC{Float32}(A_lpt))
+    c = similar(b, length(b_test))
+    @benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
 end
 
 let b = CuArray{Float32}(b_test_left[:, 1:1])
-	A = CuSparseMatrixCSR(SparseMatrixCSC{Float32}(A_lpt))
-	c = similar(b, length(b_test), 1)
-	@benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+    A = CuSparseMatrixCSR(SparseMatrixCSC{Float32}(A_lpt))
+    c = similar(b, length(b_test), 1)
+    @benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
 end
 
 let b = CuArray{Float32}(b_test)
-	A = adapt(CUDABackend(), A_lp)
-	c = similar(b, length(b_test_left))
-	@benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+    A = adapt(CUDABackend(), A_lp)
+    c = similar(b, length(b_test_left))
+    @benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
 end
 
 let b = CuArray{Float32}(b_test)
-	A = adapt(CUDABackend(), JDSMatrix{Float32}(A_lp))
-	c = similar(b, length(b_test_left))
-	@benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+    b2 = similar(b)
+    A = adapt(CUDABackend(), A_lp)
+    c = similar(b, length(b_test_left))
+    s1, s2 = CUDA.ones.(size(A))
+    @benchmark CUDA.@sync begin
+        $b2 .= $s2 .* $b
+        mul!($c, $A, $b2, 1f0, 0f0)
+        $c .*= $s1
+    end
 end
 
 let b = CuArray{Float32}(b_test)
-	A = CuSparseMatrixCSR(SparseMatrixCSC{Float32}(A_lp))
-	c = similar(b, length(b_test_left))
-	@benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+    A = adapt(CUDABackend(), JDSMatrix{Float32}(A_lp))
+    c = similar(b, length(b_test_left))
+    @benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+end
+
+let b = CuArray{Float32}(b_test)
+    A = CuSparseMatrixCSR(SparseMatrixCSC{Float32}(A_lp))
+    c = similar(b, length(b_test_left))
+    @benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
 end
 
 let b = CuArray{Float32}(b_test[:, 1:1])
-	A = CuSparseMatrixCSR(SparseMatrixCSC{Float32}(A_lp))
-	c = similar(b, length(b_test_left), 1)
-	@benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
+    A = CuSparseMatrixCSR(SparseMatrixCSC{Float32}(A_lp))
+    c = similar(b, length(b_test_left), 1)
+    @benchmark CUDA.@sync mul!($c, $A, $b, 1f0, 0f0)
 end
